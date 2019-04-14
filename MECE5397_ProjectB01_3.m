cfcf6d52ea386980e7802132d2a56b1ed71e3190
jdfrady <48896855+jdfrady@users.noreply.github.com>
2019-04-14 10:38:48 -0500

    Add files via upload

---
+++ MECE5397_ProjectB01_3.m
@@ -0,0 +1,32 @@
+%% This code compares ....
+
+%% User Inputs
+t = 3;   % Simulation Time
+nt = 5;  % Number of time steps
+ny = 3;  % Number of y steps 
+nx = 3;  % Number of x steps
+ax = 0;  % Value of lower x limit
+bx = 0;  % Value of upper x limit
+ay = 0;  % Value of lower y limit
+by = 0;  % Value of upper y limit
+
+%% Definitions and Initializatons
+dx = (bx-ax)/(nx+1);          % X step size
+dy = (by-ay)/(ny+1);          % Y step Size
+dt = t/(nt+1);                % Time step size
+u = zeros(nx+1, ny+1, nt+1);  % Allocating memory 
+
+
+%% Boundary Condidions
+temp 1
+
+
+%% Explicit Calculation
+
+
+
+%% Exact Solution
+
+
+%% Visualization
+
